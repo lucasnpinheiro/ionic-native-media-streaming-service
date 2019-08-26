801b78bfb199e7b706a5b2042af4d9103a87e1b7
Paul Kibe Njuguna <wacujuguna@gmail.com>
2019-08-26 17:27:53 -0300

    Add media stream status

--- index.d.ts
+++ index.d.ts
@@ -6,34 +6,13 @@ export interface MediaStream {
     description: string;
     cover: string;
 }
-/**
- * @name Media Streaming Service
- * @description
- * This plugin does something
- *
- * @usage
- * ```typescript
- * import { MediaStreamingService } from '@ionic-native/media-streaming-service';
- *
- *
- * constructor(private mediaStreamingService: MediaStreamingService) { }
- *
- * ...
- *
- *
- * this.mediaStreamingService.functionName('Hello', 123)
- *   .then((res: any) => console.log(res))
- *   .catch((error: any) => console.error(error));
- *
- * ```
- */
+export declare enum MediaPlayerState {
+    play = 0,
+    pause = 1,
+    stop = 2,
+    close = 3
+}
 export declare class MediaStreamingServiceOriginal extends IonicNativePlugin {
-    /**
-     * This function does something
-     * @param arg1 {string} Some param to configure something
-     * @param arg2 {number} Another param to configure something
-     * @return {Promise<any>} Returns a promise that resolves when something happens
-     */
     start(channelId: string, channelName: string, notificationId: string, mediaStreams: MediaStream[], selectedIndex: string): Promise<any>;
     play(selectedIndex: string): Promise<any>;
     pause(): Promise<any>;
