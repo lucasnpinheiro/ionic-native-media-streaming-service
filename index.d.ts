1cf02e98d20a888ff9d15760e7cf9c4f3695c1a8
Paul Kibe Njuguna <wacujuguna@gmail.com>
2019-08-27 10:19:21 -0300

    Add close action

--- index.d.ts
+++ index.d.ts
@@ -14,16 +14,18 @@ export interface MediaStreamRequest {
     selectedIndex: string;
 }
 export declare enum MediaPlayerState {
-    play = 0,
-    pause = 1,
-    stop = 2,
-    close = 3
+    start = 0,
+    play = 1,
+    pause = 2,
+    stop = 3,
+    close = 4
 }
 export declare class MediaStreamingServiceOriginal extends IonicNativePlugin {
     start(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     play(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     pause(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     stop(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
+    close(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
 }
 
 export declare const MediaStreamingService: MediaStreamingServiceOriginal;
--- ngx/index.d.ts
+++ ngx/index.d.ts
@@ -14,14 +14,16 @@ export interface MediaStreamRequest {
     selectedIndex: string;
 }
 export declare enum MediaPlayerState {
-    play = 0,
-    pause = 1,
-    stop = 2,
-    close = 3
+    start = 0,
+    play = 1,
+    pause = 2,
+    stop = 3,
+    close = 4
 }
 export declare class MediaStreamingService extends IonicNativePlugin {
     start(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     play(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     pause(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
     stop(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
+    close(options: MediaStreamRequest, successCallback?: Function, errorCallback?: Function): void;
 }
